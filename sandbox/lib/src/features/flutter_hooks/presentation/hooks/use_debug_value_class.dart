7b4353ae9ea07e67e3a22370b7507a16e15eb43c
Karlo M <karlo.marinovic@decode.agency>
2023-03-10 14:56:46 +0100

    backup

--- sandbox/lib/src/features/flutter_hooks/presentation/hooks/use_debug_value_class.dart
+++ sandbox/lib/src/features/flutter_hooks/presentation/hooks/use_debug_value_class.dart
@@ -1,5 +1,11 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+
+ValueNotifier<T> useDebugValueWithClass<T>(T initialData, String debugLabel) {
+  return use(_DebugValueHook(initialData: initialData, debugLabel: debugLabel));
+}
 
 class _DebugValueHook<T> extends Hook<ValueNotifier<T>> {
   const _DebugValueHook({
@@ -9,4 +15,35 @@ class _DebugValueHook<T> extends Hook<ValueNotifier<T>> {
 
   final String debugLabel;
   final T initialData;
+
+  // @override
+  // HookState<ValueNotifier<T>, Hook<ValueNotifier<T>>> createState() {
+  //   // TODO: implement createState
+  //   throw UnimplementedError();
+  // }
+
+  @override
+  _DebugValueHookState<T> createState() => _DebugValueHookState<T>();
+}
+
+class _DebugValueHookState<T>
+    extends HookState<ValueNotifier<T>, _DebugValueHook<T>> {
+  late final _state = ValueNotifier<T>(hook.initialData)
+    ..addListener(_listener);
+
+  @override
+  void dispose() {
+    _state.dispose();
+  }
+
+  @override
+  ValueNotifier<T> build(BuildContext contet) {
+    log("${hook.debugLabel} : ${_state.value}");
+
+    return _state;
+  }
+
+  void _listener() {
+    setState(() {});
+  }
 }
